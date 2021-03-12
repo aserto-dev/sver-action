63d9b693da939b86e82833a6f218d3192fa8267d
viovanov <viovanov@suse.com>
2021-03-12 20:53:28 +0200

    Properly set the output value

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
+set -e
+
 export PRE_RELEASE="$INPUT_PRE_RELEASE"
 
 if [ -n "${INPUT_DOCKER_IMAGE}" ]; then
-  exec /app/calc-version tags -s "${INPUT_DOCKER_REGISTRY}" -u "${INPUT_DOCKER_USERNAME}" -p "${INPUT_DOCKER_PASSWORD}" "${INPUT_DOCKER_IMAGE}"
+  version=$(/app/calc-version tags -s "${INPUT_DOCKER_REGISTRY}" -u "${INPUT_DOCKER_USERNAME}" -p "${INPUT_DOCKER_PASSWORD}" "${INPUT_DOCKER_IMAGE}")
+else if [ -n "${INPUT_NEXT}"]; then
+  version=$(/app/calc-version --next "${INPUT_NEXT}")
+else
+  version=$(/app/calc-version)
 fi
 
-if [ -n "${INPUT_NEXT}"]; then
-  exec /app/calc-version --next "${INPUT_NEXT}"
-fi
-
-exec /app/calc-version
+echo "::set-output name=version::$version"
