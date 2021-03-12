66f7d2c6e4afeb1e6aa40ac75e790a17cb090339
viovanov <viovanov@suse.com>
2021-03-12 19:11:38 +0200

    Fix input variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+export PRE_RELEASE="$INPUT_PRE_RELEASE"
 
-if [ -n "${DOCKER_IMAGE}" ]; then
-  exec /app/calc-version tags -s "${DOCKER_REGISTRY}" -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}" "${DOCKER_IMAGE}"
+if [ -n "${INPUT_DOCKER_IMAGE}" ]; then
+  exec /app/calc-version tags -s "${INPUT_DOCKER_REGISTRY}" -u "${INPUT_DOCKER_USERNAME}" -p "${INPUT_DOCKER_PASSWORD}" "${INPUT_DOCKER_IMAGE}"
 fi
 
-if [ -n "${NEXT}"]; then
-  exec /app/calc-version --next "${NEXT}"
+if [ -n "${INPUT_NEXT}"]; then
+  exec /app/calc-version --next "${INPUT_NEXT}"
 fi
 
 exec /app/calc-version
