7978e526a0ec15c435c279fef1b856d7f1e992b9
viovanov <viovanov@suse.com>
2021-03-12 21:00:24 +0200

    Fix bash script error

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ export PRE_RELEASE="$INPUT_PRE_RELEASE"
 
 if [ -n "${INPUT_DOCKER_IMAGE}" ]; then
   version=$(/app/calc-version tags -s "${INPUT_DOCKER_REGISTRY}" -u "${INPUT_DOCKER_USERNAME}" -p "${INPUT_DOCKER_PASSWORD}" "${INPUT_DOCKER_IMAGE}")
-else if [ -n "${INPUT_NEXT}"]; then
+elif [ -n "${INPUT_NEXT}"]; then
   version=$(/app/calc-version --next "${INPUT_NEXT}")
 else
   version=$(/app/calc-version)
